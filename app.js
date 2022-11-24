3f06d8798934ff60839e0382daf6c4f9a525676e
Thomas Månsson <ThomasCreate@ThomasCreate.local>
2022-11-24 10:00:36 +0100

    add code

---
+++ app.js
@@ -0,0 +1,30 @@
+/* function foo() {
+  console.log("test");
+}
+foo("hej"); */
+
+/* let a = foo(3);
+console.log(a);
+function foo(i) {
+return i * i;
+} */
+
+/* console.log(foo(3, 5));
+function foo(x, y) {
+    return x * y;
+} */
+
+/* let x = 5;
+let y = 10;
+
+function magic() {
+    let x = 10;
+
+    y = 20;
+
+    console.log(x);
+}
+
+magic();
+
+console.log('x == ' + x + ' , y == ' + y); */
