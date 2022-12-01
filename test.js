da6ce87653b283565a28c39c56074a613a56000c
LukasCJ <93259361+LukasCJ@users.noreply.github.com>
2022-12-01 09:52:01 +0100

    jut5o5tf

---
+++ test.js
@@ -0,0 +1,15 @@
+function foo() {
+console.log("test");
+}
+
+foo();
+
+let arr = [24, 33, 77];
+arr.forEach((val) => console.log(val));
+
+let arr2 = [235,4679,145,7,12];
+let i = 0;
+while(i < arr2.length) {
+    console.log(arr2[i]);
+    i++;
+}
